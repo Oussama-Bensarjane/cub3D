50465dbdd0b475904bc98b567ab9385261f9d01e
Mohamed El Hamidi <mel-hami@e1r10p1.1337.ma>
2025-09-11 18:37:48 +0100

    [UPDATE] config.h
    - add ASCII code for APPLE (so game can be played in both Mac and Linux)

--- includes/config.h
+++ includes/config.h
@@ -11,13 +11,23 @@
 # define FOV_R (PI / 3)
 # define SPEED 1
 
-# define ESC 65307
-# define W 119
-# define A 97
-# define S 115
-# define D 100
-# define LEFT 65361
-# define RIGHT 65363
+ #if defined(__linux__)
+  # define ESC 65307
+  # define W 119
+  # define A 97
+  # define S 115
+  # define D 100
+  # define LEFT 65361
+  # define RIGHT 65363
+ #elif defined(__APPLE__)
+  # define ESC 53
+  # define W 13
+  # define A 0
+  # define S 1
+  # define D 2
+  # define LEFT 123
+  # define RIGHT 124
+ #endif
 
 typedef struct	s_player
 {
