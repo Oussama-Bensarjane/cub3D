3c872d520c5947a469f8760f27912611fa032459
Mohamed El Hamidi <mel-hami@e2r12p3.1337.ma>
2025-09-29 21:10:06 +0100

    [UPDATE] exit_free.c
    - define free_textures_paths

--- src/utils/exit_free.c
+++ src/utils/exit_free.c
@@ -21,18 +21,23 @@ void	free_2d_array(char **arr)
 	free(arr);
 }
 
-void	free_assets(t_assets *cfg)
+void	free_textures_paths(char *paths[TEX_MAX])
 {
 	int	i;
 
 	i = 0;
 	while (i < TEX_MAX)
 	{
-		if (cfg->textures[i])
-			free(cfg->textures[i]);
-		cfg->textures[i] = NULL;
+		if (paths[i])
+			free(paths[i]);
+		paths[i] = NULL;
 		i++;
 	}
+}
+
+void	free_assets(t_assets *cfg)
+{
+	free_textures_paths(cfg->textures);
 	if (cfg->map)
 		free_2d_array(cfg->map);
 	cfg->map = NULL;
