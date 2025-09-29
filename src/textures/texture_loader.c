49e524da4795a415b51c6dd43a1fa6f88aaad987
Mohamed El Hamidi <mel-hami@e2r12p3.1337.ma>
2025-09-29 21:10:44 +0100

    [UPDATE] texture_loader.c
    - call free_textures_paths

--- src/textures/texture_loader.c
+++ src/textures/texture_loader.c
@@ -25,16 +25,21 @@ static void	load_textures(t_game *game, char *paths[TEX_MAX])
 	{
 		textures[i].img.img = mlx_xpm_file_to_image(game->mlx, paths[i], \
 &textures[i].width, &textures[i].height);
-		free(paths[i]);
-		paths[i] = NULL;
 		if (!textures[i].img.img)
+		{
+			free_textures_paths(paths);
 			game_over(game, "Error: Failed to load texture file", EXIT_FAILURE);
+		}
 		textures[i].img.data = mlx_get_data_addr(textures[i].img.img, \
 &textures[i].img.bpp, &textures[i].img.size_line, &textures[i].img.endian);
 		if (!textures[i].img.data)
+		{
+			free_textures_paths(paths);
 			game_over(game, "Error: Failed to get texture data", EXIT_FAILURE);
+		}
 		i++;
 	}
+	free_textures_paths(paths);
 }
 
 void	init_load_textures(t_game *game, char *textures_paths[TEX_MAX])
