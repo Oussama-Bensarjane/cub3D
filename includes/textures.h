f6426a33256b6405cfb98ed616717b07a5a0c9a4
Mohamed El Hamidi <mel-hami@e2r12p3.1337.ma>
2025-09-29 21:13:44 +0100

    [RESTORE] free_textures_paths from main

--- includes/textures.h
+++ includes/textures.h
@@ -8,4 +8,5 @@ double	calc_tex_step(int text_height, int line_h, int start);
 void	draw_ceiling(int x, int start_y, int end_y, t_game *game);
 void	draw_wall(int x, t_game *game);
 void	draw_floor(int x, int start_y, int end_y, t_game *game);
+void	free_textures_paths(char *paths[TEX_MAX]);
 #endif
