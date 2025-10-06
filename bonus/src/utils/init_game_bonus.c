765fc46175aa9089f453e051e6fe39495235ebeb
Mohamed El Hamidi <mel-hami@e1r9p1.1337.ma>
2025-10-06 13:56:33 +0100

    [UPDATE] init_game_bonus.c
    - Add add_door
    - Fix Norminette

--- bonus/src/utils/init_game_bonus.c
+++ bonus/src/utils/init_game_bonus.c
@@ -1,5 +1,19 @@
 #include "cub3d_bonus.h"
 
+static void	add_door(t_game *game, int x, int y)
+{
+	t_door	*door;
+
+	door = malloc(sizeof(t_door));
+	if (!door)
+		game_over(game, "Error: Failed to malloc door!", EXIT_FAILURE);
+	door->x = x;
+	door->y = y;
+	door->is_open = 0;
+	door->anim = 0;
+	ft_lstadd_back(&game->doors, ft_lstnew(door));
+}
+
 static void	init_config(t_game *game, t_assets *assets)
 {
 	t_config	*config;
@@ -11,32 +25,21 @@ static void	init_config(t_game *game, t_assets *assets)
 	config->map_height = assets->map_height;
 	config->map_width = malloc(config->map_height * sizeof(int));
 	if (!config->map_width)
-		game_over(game, "Error: Failed to init_config \
-[cannot mallocated config->map_width]!", EXIT_FAILURE);
+		game_over(game,
+			"Error: Failed to init_config [malloc config->map_width]!",
+			EXIT_FAILURE);
 	game->doors = NULL;
-	i = 0;
-	while (i < config->map_height)
+	i = -1;
+	while (++i < config->map_height)
 	{
 		config->map_width[i] = (int)ft_strlen(config->map[i]);
-		j = 0;
-		while (j < config->map_width[i])
+		j = -1;
+		while (++j < config->map_width[i])
 		{
 			if (config->map[i][j] == 'D')
-			{
-				t_door *door = malloc(sizeof(t_door));
-				door->x = j;
-				door->y = i;
-				door->is_open = 0;
-				door->anim = 0;
-				door->speed = DOOR_OPEN_CLOSE_SPEED;
-				ft_lstadd_back(&game->doors, ft_lstnew(door));
-			}
-			j++;
+				add_door(game, j, i);
 		}
-		i++;
 	}
-	game->img.img = NULL;
-	game->win = NULL;
 }
 
 static void	init_ceiling_floor_colors(t_game *game, t_assets *assets)
@@ -67,13 +70,14 @@ void	init_game(t_game *game, t_assets *assets)
 		exit_free(assets, "Error: Cannot initialize Cub3D!");
 	if (!assets->map)
 		exit_free(assets, "Error: Cannot load the map!");
+	game->img.img = NULL;
+	game->win = NULL;
 	init_config(game, assets);
 	init_player(&game->player, assets);
 	game->mlx = mlx_init();
 	if (!game->mlx)
 		game_over(game, "Error: Failed to initialize mlx!", EXIT_FAILURE);
-	init_load_textures(game, assets->textures);
-	init_load_weapons(game);
+	(init_load_textures(game, assets->textures), init_load_weapons(game));
 	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "Cub3D");
 	if (!game->win)
 		game_over(game, "Error: mlx_new_window failed!", EXIT_FAILURE);
