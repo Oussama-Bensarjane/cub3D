dd1656d5abd47610168ae81b61fdbabbd46774b5
Oussama <81320123+Oussama-Bensarjane@users.noreply.github.com>
2025-10-08 19:50:49 +0100

    [Update] sprites_bonus.h
    add macros define nmr of frames per weapon

--- bonus/includes/sprites_bonus.h
+++ bonus/includes/sprites_bonus.h
@@ -1,6 +1,10 @@
 #ifndef SPRITES_BONUS_H
 # define SPRITES_BONUS_H
 
+# define HAND_ATTACK_FRAMES 3
+# define PISTOL_ATTACK_FRAMES 7
+# define SHOTGUN_ATTACK_FRAMES 17
+
 void	init_load_weapons(t_game *game);
 void	draw_weapon(t_game *game);
 void	update_sprites(t_sprite *sprite);
